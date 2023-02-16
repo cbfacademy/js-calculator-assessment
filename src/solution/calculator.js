5df763ca527840cf3c8d9c35d4e4afc7a3db2661
Genyus <genyus@gmail.com>
2023-02-16 02:13:07 +0000

    refactor: strips all DOM refs from Calculator Extracted all DOM references to enable cleaner testing

--- src/solution/calculator.js
+++ src/solution/calculator.js
@@ -1,145 +1,108 @@
 class Calculator {
-    constructor() {
-      this.symbolButtons = document.querySelectorAll("[data-role='symbol']");
-      this.operatorButtons = document.querySelectorAll("[data-role='operator']");
-      this.equalsButton = document.querySelector("[data-role='equals']");
-      this.deleteButton = document.querySelector("[data-role='delete']");
-      this.allClearButton = document.querySelector("[data-role='all-clear']");
-      this.previousOperandElement = document.querySelector(
-        "[data-role='previous-operand']"
-      );
-      this.currentOperandElement = document.querySelector(
-        "[data-role='current-operand']"
-      );
-      this.clear();
-      this.#addHandlers();
-    }
-  
-    #addHandlers() {
-      const calculator = this;
-  
-      document.addEventListener("keydown", function (event) {
-        let symbolPattern = /[0-9.]/g;
-        let operatorPattern = /[+\-*\/]/g;
-  
-        if (event.key.match(symbolPattern)) {
-          event.preventDefault();
-          calculator.appendSymbol(event.key);
-        }
-        if (event.key.match(operatorPattern)) {
-          event.preventDefault();
-          calculator.apply(event.key);
-        }
-        if (event.key === "Enter" || event.key === "=") {
-          event.preventDefault();
-          calculator.calculate();
-        }
-        if (event.key === "Backspace") {
-          event.preventDefault();
-          calculator.deleteSymbol();
-        }
-        if (event.key === "Delete" || event.key === "Escape") {
-          event.preventDefault();
-          calculator.clear();
-        }
+  constructor(emitRefresh) {
+    this.#emitRefresh = emitRefresh;
+    this.clearDisplay();
+  }
+
+  #format(number) {
+    const numericString = number.toString();
+    const integerDigits = parseFloat(numericString.split(".")[0]);
+    const decimalDigits = numericString.split(".")[1];
+    let integerDisplay;
+
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
       });
     }
-  
-    #updateDisplay() {
-      this.currentOperandElement.innerText = this.format(this.currentOperand);
-  
-      if (this.operator != null) {
-        this.previousOperandElement.innerText = `${this.format(
-          this.previousOperand
-        )} ${this.operator}`;
-      } else {
-        this.previousOperandElement.innerText = "";
-      }
-    }
-  
-    clear() {
-      this.currentOperand = "";
-      this.previousOperand = "";
-      this.operator = undefined;
-      this.#updateDisplay();
-    }
-  
-    deleteSymbol() {
-      this.currentOperand = this.currentOperand.toString().slice(0, -1);
-      this.#updateDisplay();
-    }
-  
-    appendSymbol(symbol) {
-      if (symbol === "." && this.currentOperand.includes(".")) return;
-  
-      this.currentOperand = this.currentOperand.toString() + symbol.toString();
-      this.#updateDisplay();
-    }
-  
-    apply(operator) {
-      if (this.currentOperand === "") return;
-  
-      if (this.previousOperand !== "") {
-        this.calculate();
-      }
-  
-      this.operator = operator;
-      this.previousOperand = this.currentOperand;
-      this.currentOperand = "";
-      this.#updateDisplay();
-    }
-  
-    calculate() {
-      let result;
-      const prev = parseFloat(this.previousOperand);
-      const current = parseFloat(this.currentOperand);
-  
-      if (isNaN(prev) || isNaN(current)) return;
-  
-      switch (this.operator) {
-        case "+":
-          result = prev + current;
-          break;
-        case "-":
-          result = prev - current;
-          break;
-        case "*":
-          result = prev * current;
-          break;
-        case "/":
-        case "÷":
-          result = prev / current;
-          break;
-        default:
-          return;
-      }
-  
-      this.currentOperand = result;
-      this.operator = undefined;
-      this.previousOperand = "";
-      this.#updateDisplay();
-    }
-  
-    format(number) {
-      const numericString = number.toString();
-      const integerDigits = parseFloat(numericString.split(".")[0]);
-      const decimalDigits = numericString.split(".")[1];
-      let integerDisplay;
-  
-      if (isNaN(integerDigits)) {
-        integerDisplay = "";
-      } else {
-        integerDisplay = integerDigits.toLocaleString("en", {
-          maximumFractionDigits: 0,
-        });
-      }
-  
-      if (decimalDigits != null) {
-        return `${integerDisplay}.${decimalDigits}`;
-      } else {
-        return integerDisplay;
-      }
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
     }
   }
-  
-  export default new Calculator();
+
+  #getDisplayData() {
+    const currentOperand = this.#format(this.currentOperand);
+    const previousOperand =
+      this.operator != null
+        ? `${this.#format(this.previousOperand)} ${this.operator}`
+        : "";
+
+    return {
+      detail: { current: currentOperand, previous: previousOperand },
+    };
+  }
+
+  #refreshDisplay() {
+    this.#emitRefresh(this.#getDisplayData())
+  }
+
+  calculate() {
+    let result;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (this.operator) {
+      case "+":
+        result = prev + current;
+        break;
+      case "-":
+        result = prev - current;
+        break;
+      case "*":
+        result = prev * current;
+        break;
+      case "/":
+      case "÷":
+        result = prev / current;
+        break;
+      default:
+        return;
+    }
+
+    this.currentOperand = result;
+    this.operator = undefined;
+    this.previousOperand = "";
+    this.#refreshDisplay();
+  }
+
+  clearDisplay() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operator = undefined;
+    this.#refreshDisplay();
+  }
+
+  deleteSymbol() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    this.#refreshDisplay();
+  }
+
+  readOperator(operator) {
+    if (this.currentOperand === "") return;
+
+    if (this.previousOperand !== "") {
+      this.calculate();
+    }
+
+    this.operator = operator;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+    this.#refreshDisplay();
+  }
+
+  readSymbol(symbol) {
+    if (symbol === "." && this.currentOperand.includes(".")) return;
+
+    this.currentOperand = this.currentOperand.toString() + symbol.toString();
+    this.#refreshDisplay();
+  }
+}
+
+export default Calculator;
--- src/solution/script.js
+++ src/solution/script.js
@@ -1,28 +1,81 @@
-import calculator from "./calculator.js";
+import Calculator from "./calculator.js";
+
+const displayElement = document.querySelector(".display");
+const calculator = new Calculator((data) => {
+  displayElement.dispatchEvent(
+    new CustomEvent("refresh", data)
+  );
+});
+const symbolButtons = document.querySelectorAll("[data-role='symbol']");
+const operatorButtons = document.querySelectorAll("[data-role='operator']");
+const equalsButton = document.querySelector("[data-role='equals']");
+const deleteButton = document.querySelector("[data-role='delete']");
+const allClearButton = document.querySelector("[data-role='all-clear']");
+const previousOperandElement = document.querySelector(
+  "[data-role='previous-operand']"
+);
+const currentOperandElement = document.querySelector(
+  "[data-role='current-operand']"
+);
+displayElement.addEventListener("refresh", (event) => {
+  const { current, previous } = event.detail;
+  if (typeof current === "string") {
+    currentOperandElement.innerText = current;
+  }
+  if (typeof previous === "string") {
+    previousOperandElement.innerText = previous;
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  let symbolPattern = /[0-9.]/g;
+  let operatorPattern = /[+\-*\/]/g;
+
+  if (event.key.match(symbolPattern)) {
+    event.preventDefault();
+    calculator.readSymbol(event.key);
+  }
+  if (event.key.match(operatorPattern)) {
+    event.preventDefault();
+    calculator.readOperator(event.key);
+  }
+  if (event.key === "Enter" || event.key === "=") {
+    event.preventDefault();
+    calculator.calculate();
+  }
+  if (event.key === "Backspace") {
+    event.preventDefault();
+    calculator.deleteSymbol();
+  }
+  if (event.key === "Delete" || event.key === "Escape") {
+    event.preventDefault();
+    calculator.clearDisplay();
+  }
+});
 
 /**
  * Your code goes here
  */
-calculator.symbolButtons.forEach((button) => {
+symbolButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    calculator.appendSymbol(button.innerText);
+    calculator.readSymbol(button.innerText);
   });
 });
 
-calculator.operatorButtons.forEach((button) => {
+operatorButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    calculator.apply(button.innerText);
+    calculator.readOperator(button.innerText);
   });
 });
 
-calculator.equalsButton.addEventListener("click", (button) => {
+equalsButton.addEventListener("click", (button) => {
   calculator.calculate();
 });
 
-calculator.allClearButton.addEventListener("click", (button) => {
-  calculator.clear();
+allClearButton.addEventListener("click", (button) => {
+  calculator.clearDisplay();
 });
 
-calculator.deleteButton.addEventListener("click", (button) => {
+deleteButton.addEventListener("click", (button) => {
   calculator.deleteSymbol();
 });
